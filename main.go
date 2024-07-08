f53b8421cffe40fd1f70c98bb07dea3cb94815c6
Raajeev Chandran <raajeevchandran787@gmail.com>
2024-07-08 14:07:20 +0530

    http server

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"sync"
 
+	"github.com/gorilla/mux"
+)
+
+var urlStore = struct {
+	sync.RWMutex
+	urls map[string]string
+}{urls: make(map[string]string)}
+
+func createShortURL(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func redirectURL(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/create", createShortURL).Methods("POST")
+	r.HandleFunc("/{shortURL}", redirectURL).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
 }
