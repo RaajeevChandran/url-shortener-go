30158e29e9c6a5d4b5e57bf3966bcc5e3e4b37f2
Raajeev Chandran <raajeevchandran787@gmail.com>
2024-07-08 14:36:49 +0530

    implemented createShortURL & redirectURL routes

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"math/rand"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -12,12 +15,52 @@ var urlStore = struct {
 	urls map[string]string
 }{urls: make(map[string]string)}
 
-func createShortURL(w http.ResponseWriter, r *http.Request) {
+func generateShortURL(n int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
 
+func createShortURL(w http.ResponseWriter, r *http.Request) {
+	var requestData struct {
+		URL string `json:"url"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	shortURL := generateShortURL(6)
+	urlStore.Lock()
+	urlStore.urls[shortURL] = requestData.URL
+	urlStore.Unlock()
+
+	responseData := struct {
+		ShortURL string `json:"short_url"`
+	}{
+		ShortURL: r.Host + "/" + shortURL,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(responseData)
 }
 
 func redirectURL(w http.ResponseWriter, r *http.Request) {
-	
+	vars := mux.Vars(r)
+	shortURL := vars["shortURL"]
+
+	urlStore.RLock()
+	originalURL, ok := urlStore.urls[shortURL]
+	urlStore.RUnlock()
+
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+	http.Redirect(w, r, originalURL, http.StatusFound)
 }
 
 func main() {
